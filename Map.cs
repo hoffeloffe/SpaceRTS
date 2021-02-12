b353e9877e65cf1f61dd6cbd4fea45437909f9d1
Hoffe <christian.hoffmann.thomsen@gmail.com>
2021-02-12 11:32:00 +0100

    Klasser

---
+++ Map.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceRTS
+{
+    internal class Map
+    {
+        private int rows = 5;
+        private int cols = 5;
+
+        private void MakeGrid()
+        {
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                }
+            }
+        }
+    }
+}
